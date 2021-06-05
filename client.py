c80b3ceffca2c6ca03fbbfa797d0669fb741e097
shashwat052000 <71658650+shashwat052000@users.noreply.github.com>
2021-06-06 00:51:05 +0530

    Update client.py

--- client.py
+++ client.py
@@ -11,18 +11,18 @@ while True:
     while len(data) < payload_size:
         packet = client_socket.recv(4*1024) # 4K
         if not packet: break
-        data+=packet
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("Q",packed_msg_size)[0]
-    
-    while len(data) < msg_size:
-        data += client_socket.recv(4*1024)
-    frame_data = data[:msg_size]
-    data  = data[msg_size:]
-    frame = pickle.loads(frame_data)
-    cv2.imshow("RECEIVING VIDEO",frame)
-    key = cv2.waitKey(13) & 0xFF
-    if key  == ord('q'):
-        break
+            data+=packet
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("Q",packed_msg_size)[0]
+
+            while len(data) < msg_size:
+                data += client_socket.recv(4*1024)
+                frame_data = data[:msg_size]
+                data  = data[msg_size:]
+                frame = pickle.loads(frame_data)
+                cv2.imshow("RECEIVING VIDEO",frame)
+                key = cv2.waitKey(13) & 0xFF
+                if key  == ord('q'):
+                    break
 client_socket.close()
